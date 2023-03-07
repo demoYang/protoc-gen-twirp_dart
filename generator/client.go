767528817cbd2bdf0e26cf9e5f8b91824240c04a
xuyang <xuyang@qimao.com>
2023-03-07 17:01:39 +0800

    feature(fix): change code

--- generator/client.go
+++ generator/client.go
@@ -131,7 +131,7 @@ class {{.Name}} {
 		{{- else if and (.IsMessage) (eq .Type "DateTime")}}
 		map['{{.JSONName}}'] = {{.Name}}.toIso8601String();
 		{{- else if .IsMessage}}
-		map['{{.JSONName}}'] = {{.Name}}?.toJson() ?? {};
+		map['{{.JSONName}}'] = {{.Name}}?.toJson();
 		{{- else if eq .Type  "Int64" }}
 		map['{{.JSONName}}'] = {{.Name}}.toString();
 		{{- else}}
